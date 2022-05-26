d124467b103b53236ca1d8f32ae0bdabe5378a16
ChoiSungwoo1216 <97146749+ChoiSungwoo1216@users.noreply.github.com>
2022-05-26 15:24:47 +0900

    Add files via upload
    불필요 스타일 삭제, reset 기능 수정

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { Route } from "react-router-dom";
+
+import WeekList from "./WeekList";
+import Review from "./Review";
+
+function App() {
+
+  let now = new Date();
+  const week = ["일", "월", "화", "수", "목", "금", "토", "일", "월", "화", "수", "목", "금"];
+  
+  const [list] = React.useState([`${week[now.getDay()]}`, `${week[(now).getDay() + 1]}`, `${week[(now).getDay() + 2]}`, `${week[(now).getDay() + 3]}`, `${week[(now).getDay() + 4]}`, `${week[(now).getDay() + 5]}`, `${week[(now).getDay() + 6]}`]);
+
+
+  return (
+    <div className="App">
+      <Container>
+        <Route path="/" 
+        exact 
+        render = {(props) => <WeekList list= {list} />}
+        />
+        <Route path="/review/:id"
+        exact
+        render = {(props)=><Review list={list}/>}
+        />
+      </Container>
+    </div>
+  );
+}
+
+
+const Container = styled.div`
+max-width: 350px;
+min-height: 90vh;
+margin: 5vh auto;
+padding: 5vh 0px;
+border: 1px solid rgb(221, 221, 221);
+box-sizing: border-box;
+border-radius: 5px;
+`;
+
+export default App;
